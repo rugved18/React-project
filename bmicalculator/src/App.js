171010a28e737a019cc5a811622c26343f859bcc
rugved18 <rugvedkulk2003@gmail.com>
2024-06-03 00:34:28 +0530

    final 02-06-2024

--- bmicalculator/src/App.js
+++ bmicalculator/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+
+  const[height,setHeight] =useState(70);
+  const[weight,setWeight] =useState(180);
+  const[bmi,setBmi] =useState('');
+
+  function changeweight(event){
+    setWeight(event.target.value);
+  }
+
+  function changeHeight(event){
+    setHeight(event.target.value);
+  }
+    
+  const output =()=>{
+    const heightInMeters = height / 100;
+    const calculatedBmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+    setBmi(calculatedBmi);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='outer-container'>
+      <h1>BMI Calculator</h1>
+
+  <div>
+  <p className='input-section'  >weight is {weight} kg</p>
+  <input className='input-section' type='range' step={1} min={40} max={200} onChange={changeweight}></input>
+  
+  <p className='input-section' >height is {height} cm</p>
+  <input className='input-section' type='range' step={1} min={40} max={200}  onChange={changeHeight}></input>
+
+  <p className='output-selction'> the BMI is {bmi} </p>
+  <button onClick={output}>click</button>
+</div>
+      </div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
